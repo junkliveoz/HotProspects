ca2ab5273845a9bbf5c887e9336baca7d2533695
Adam Sayer <asayer82@icloud.com>
2024-08-28 21:46:22 +1000

    Added my version of the challenges

---
+++ HotProspects/DetailedProspectView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailedProspectView.swift
+//  HotProspects
+//
+//  Created by Adam on 28/8/2024.
+//
+
+import SwiftUI
+
+struct DetailedProspectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailedProspectView()
+}
